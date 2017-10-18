9d4de1f9fb2a087b56a9d72ce22318f41826a378
ferrata <al.goloshchapov@gmail.com>
2017-10-17 21:15:49 -0500

    readme

--- readme.md
+++ readme.md
@@ -1,1 +1,4 @@
-## VSCode editor settings
+## VS Code editor settings
+
+* Install https://code.visualstudio.com/insiders
+* Run `setup.py`
--- setup.py
+++ setup.py
@@ -3,5 +3,3 @@ import shutil
 
 settings_folder = os.path.join( os.environ['APPDATA'], 'Code - Insiders', 'User' )
 shutil.copytree( './settings', settings_folder )
-
-
